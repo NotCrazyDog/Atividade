3ff80cd25097da334f4e211a4a967d260242e2ff
NotCrazyDog <168197114+NotCrazyDog@users.noreply.github.com>
2024-08-15 09:56:23 -0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,5 +1,5 @@
-n1 = int(input('Digit o primeiro número: '))
-n2 = int(input('Digit o segundo número: '))
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
 print(f"{n1} + {n2} = {n1+n2}")
 print(f"{n1} - {n2} = {n1-n2}")
 print(f"{n1} . {n2} = {n1*n2}")
