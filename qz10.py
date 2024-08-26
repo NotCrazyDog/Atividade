74e1097bc9771c24caee02c1b71a3872cb3bdc03
NotCrazyDog <pedro.costa310@aluno.ce.gov.br>
2024-08-25 22:08:12 -0300

    qz10.py added

---
+++ qz10.py
@@ -0,0 +1,19 @@
+import time
+choice = ''
+i = 1
+while i <= 100:
+    print(f'{i}% - Carregando...''\n')
+    i += 1
+    time.sleep(0.25)
+while choice != '0':
+    print('''
+          ----Calculadora Python----
+          Soma - 1
+          Subtração - 2
+          Multiplicação - 3
+          Divisão - 4
+          Quadrado - 5
+          Sair - 0
+          --------------------------
+          ''')
+    choice = input('Digite sua escolha: ')
